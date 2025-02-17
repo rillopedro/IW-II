79d06fa02958513c3cd1957831a65c8f18be92e3
Pedro H. Rillo <160730098+rillopedro@users.noreply.github.com>
2025-02-17 12:29:26 -0300

    Add files via upload

---
+++ Aula02/script.js
@@ -0,0 +1,70 @@
+console.log("Exercicio 01")
+var a = 10;
+var b = 20;
+
+console.log (b);
+
+b = 5;
+
+console.log (a,b);
+
+console.log("=======================================");
+
+console.log("Exercicio 02");
+
+var a2 = 30;
+var b2 = 20;
+var c2 = (a2+b2);
+
+console.log (c2);
+
+b2 = 10;
+
+console.log (b2,c2);
+
+c2 = a2+b2;
+
+console.log (a2,b2,c2);
+
+console.log("=======================================");
+
+console.log("Exercicio 03");
+
+var a3 = 10;
+var b3 = 20;
+var c3 = a3;
+b3 = c3;
+a3 = b3;
+
+console.log(a3,b3,c3)
+
+console.log("=======================================");
+
+console.log("Exercicio 04");
+
+var a4 = 10;
+var b4 = a4+1;
+a4 = b4+1;
+b4 = a4+1;
+
+console.log(a4);
+
+a4 = b4+1;
+
+console.log(a4,b4)
+
+console.log("=======================================");
+
+console.log("Exercicio 05");
+
+var n1 = 5;
+var n2 = 10;
+var n3 = 5;
+
+mf = n1*2 + n2*3 + n3*5 ;
+
+console.log (mf)
+
+nf = mf/10
+
+console.log ("A not final é:",nf) 
